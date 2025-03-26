f39c382e6e999175e870779482442014ff5ba561
Gareth Hay <gazhay@gmail.com>
2025-03-26 12:39:30 +0000

    testing

--- pages/src/mod.mjs
+++ pages/src/mod.mjs
@@ -391,6 +391,8 @@ function testCards(){
             domDest.insertAdjacentHTML('beforeend', thisCard);
             if (j<6) {
                 document.querySelector(`div.rider[data-rider-id="${id}"]`)?.classList?.add("Group_1");
+                if (j==6)
+                document.querySelector(`div.rider[data-rider-id="${id}"]`)?.classList?.add("groupEdge");
             } else if (j>=6){
                 document.querySelector(`div.rider[data-rider-id="${id}"]`)?.classList?.add("Group_2");
             }
