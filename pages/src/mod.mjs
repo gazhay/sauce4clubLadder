5339e8d8b566f22b3601afa1dd64b8e190c5cd3c
Gareth Hay <gazhay@gmail.com>
2025-04-09 18:29:33 +0100

    fix

--- pages/src/mod.mjs
+++ pages/src/mod.mjs
@@ -13,7 +13,7 @@ console.log("ClubLadder mod ",DEBUG, SERVER);
 
 var INTERESTEDIN  = [];
 
-const scoreForPos = pos=>[10,9,8,7,6,5,4,3,2,1][pos-1]??0;
+var scoreForPos = pos=>[10,9,8,7,6,5,4,3,2,1][pos-1]??0;
 let tops        = Array.from(Array(12).keys()).map( (a,i)=>( 10+ (45*(i-1)) ));
 let riderCache    = [];
 
